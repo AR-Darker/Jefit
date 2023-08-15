d35943e533c6a037fd21331c8757ec2c475e2383
AR_Darker <ishkredov2@gmail.com>
2023-08-15 18:36:00 +0300

    Bases

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,38 @@
 package pages;
 
-public class BasePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+    public static final String URL = "";
+    public static final By BUTTON_ = By.xpath("");
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+
+    }
+    public String waitAndGetText(By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+    public void waitAndClick(By locator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).click();
+    }
+    public boolean waitAndIsDisplayedt(By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).isDisplayed();
+    }
+    public abstract boolean isPageOpen();
+
+    public boolean isExist(By locator){
+        try{
+            return driver.findElement(locator).isDisplayed();
+        }catch (NoSuchElementException e){
+            System.out.println("====>" + e.getMessage());
+            return false;
+        }
+    }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,37 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(ITestContext context){
+        WebDriverManager.chromedriver().setup();
+        /////////
+//        ChromeOptions options = new ChromeOptions();
+//        HashMap<String,Object> chromePrefs = new HashMap<>();
+//        chromePrefs.put("profile.default_content_settings.popups", 0);
+//        chromePrefs.put("download.default_directory", pathToDownload);
+//        options.setExperimentalOption("prefs",chromePrefs);
+        ///////
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        context.setAttribute("driver", driver);
+
+//todo (не забудь инициализировать страницы)
 }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+
+        driver.quit();
+    }}
