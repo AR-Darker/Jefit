b3a8829e113b31d23173686fa6675236cbb7798d
AR_Darker <ishkredov2@gmail.com>
2023-08-15 19:19:42 +0300

    Bases

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,27 +1,38 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.assertEquals;
+
 public class BaseTest {
     WebDriver driver;
+    String pathToDownload = System.getProperty("user.dir") + "";
+    String nameDownloadedFile;
+    int wait = 15000;
+    boolean fileIsNotReady = true;
     @BeforeMethod
     public void setUp(ITestContext context){
         WebDriverManager.chromedriver().setup();
         /////////
-//        ChromeOptions options = new ChromeOptions();
-//        HashMap<String,Object> chromePrefs = new HashMap<>();
-//        chromePrefs.put("profile.default_content_settings.popups", 0);
-//        chromePrefs.put("download.default_directory", pathToDownload);
-//        options.setExperimentalOption("prefs",chromePrefs);
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String,Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", pathToDownload);
+        options.setExperimentalOption("prefs",chromePrefs);
         ///////
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -30,6 +41,26 @@ public class BaseTest {
 
 //todo (не забудь инициализировать страницы)
 }
+    public void downloader(String fileLocator, String nameDownloadedFile) throws IOException, InterruptedException {
+        WebElement ourFile = driver.findElement(By.xpath(fileLocator));
+        FileUtils.cleanDirectory(new File(pathToDownload));
+        File folder = new File(pathToDownload);
+        File[] listOfFiles = folder.listFiles();
+
+        ourFile.click();
+
+        while (wait != 0 && fileIsNotReady){
+            listOfFiles =  folder.listFiles();
+            Thread.sleep(100);
+            wait-=100;
+            if ((listOfFiles.length !=0 && listOfFiles.[0].getName().equals(nameDownloadedFile)){
+                fileIsNotReady = false;
+            }
+        }
+        assertEquals(listOfFiles[0].getName(), nameDownloadedFile);
+
+
+    }
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
 
