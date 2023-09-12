2b12921222e376c550136e293db3eec309413b5e
Ivan <ishkredov2@gmail.com>
2023-09-12 16:25:43 +0300

    added --headless mode

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -12,33 +13,39 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.*;
 import utils.PropertyReader;
+import utils.TestListener;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.module.Configuration;
+
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
+@Log4j2
+@Listeners(TestListener.class)
 public class BaseTest {
-    WebDriver driver;
+    protected WebDriver driver;
     public static String downloadPath = System.getProperty("user.dir") + "\\src\\test\\resources\\filesForUploading";
-    String nameDownloadedFile;
-    String baseUrl;
     int wait = 15000;
     boolean fileIsNotReady = true;
-    public LoginPage loginPage;
-    public ProfileHomePage profileHomePage;
-    public AboutMePage aboutMePage;
-    public UserPage userPage;
-    public RoutinesPage routinesPage;
-    @BeforeMethod
+    protected LoginPage loginPage;
+    protected ProfileHomePage profileHomePage;
+    protected AboutMePage aboutMePage;
+    protected UserPage userPage;
+    protected RoutinesPage routinesPage;
+    protected CoachModePage coachModePage;
+    protected FriendsPage friendsPage;
+    protected LogWorkoutPage logWorkoutPage;
+    protected MyCustomExercisesPage myCustomExercisesPage;
+    protected MyReportsPage myReportsPage;
+    @BeforeMethod(description = "setUpBrowserParameters")
     public void setUp(ITestContext context){
-        //Configuration.baseUrl = System.getenv().getOrDefault("JEFIT_URL", PropertyReader.getProperty("jefit.url"));
-        /////////
+
         WebDriverManager.chromedriver().setup();
         HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
         chromePrefs.put("profile.default_content_settings.popups", 0);
@@ -47,7 +54,8 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", chromePrefs);
         options.addArguments("--window-size=1920,1080");
-        ///////
+        options.addArguments("--headless");
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -59,6 +67,11 @@ public class BaseTest {
         aboutMePage = new AboutMePage(driver);
         userPage = new UserPage(driver);
         routinesPage = new RoutinesPage(driver);
+        coachModePage = new CoachModePage(driver);
+        friendsPage = new FriendsPage(driver);
+        logWorkoutPage = new LogWorkoutPage(driver);
+        myCustomExercisesPage = new MyCustomExercisesPage(driver);
+        myReportsPage = new MyReportsPage(driver);
 }
     public void downloader(String fileLocator, String nameDownloadedFile) throws IOException, InterruptedException {
         WebElement ourFile = driver.findElement(By.xpath(fileLocator));
@@ -80,10 +93,20 @@ public class BaseTest {
 
 
     }
+    public void deleteAllFilesFromDirectory() {
+        File file = new File(downloadPath);
+        String[] currentFiles;
+        if (file.isDirectory()) {
+            currentFiles = file.list();
+            for (String currentFile : currentFiles) {
+                File myFile = new File(file, currentFile);
+                myFile.delete();
+            }
+        }
+    }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterMethod(alwaysRun = true, description = "Close browser")
     public void tearDown() {
-
         driver.quit();
     }}
 
