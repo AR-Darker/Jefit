349748c5a8d232d96bb41f1c28b843bb4d839883
Ivan <ishkredov2@gmail.com>
2023-09-12 16:28:51 +0300

    added LogWorkoutPage and tests

---
+++ src/test/java/pages/LogWorkoutPage.java
@@ -0,0 +1,190 @@
+package pages;
+
+import com.github.javafaker.Faker;
+import elements.ButtonAHref;
+import elements.IdButton;
+import elements.Input;
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import models.BodyStats;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+@Log4j2
+public class LogWorkoutPage extends BasePage {
+    public LogWorkoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    Faker faker = new Faker();
+    String text = faker.chuckNorris().fact();
+    String bodyStatsFieldValue = "//strong[text()=' %s : ']/parent::a";
+
+    public static final By SESSION_SUMMARY_TITLE = By.xpath("//strong[text()='Session Summary']");
+    public static final By LOG_WORKOUT_BUTTON = By.xpath("//a[normalize-space()='Log Workout']");
+    public static final By CREATE_SUMMARY_BUTTON = By.xpath("//button[text()='Create Summary']");
+    public static final By SUMMARY_IS_CREATED = By.xpath("//div[@class = 'col-12']/div[@class= 'workout-session']");
+    public static final By WEIGHT_IS_OPENED = By.xpath("//strong[text()='Body stat progress: weight( lbs):']");
+    public static final By TRAINING_IS_OPENED = By.xpath("//img[@title = 'Mark your favorite exercise. (Click again to undo)']");
+    public static final By CREATE_NOTE_BUTTON = By.xpath("//button[text()='Create Note']");
+    public static final By NOTE_TEXT = By.xpath("descendant::div[@class = 'fixed-note'][last()]");
+    public static final By NOTE_TEXT_AREA = By.xpath("//div[@class = 'add-note-layout']/form/textarea");
+    public static final By WEIGHT_BODY_STATS_BUTTON = By.xpath("//strong[text()=' Weight : ']/ancestor::a");
+
+
+    @Step("Open Log Workout page")
+    public LogWorkoutPage open() {
+        driver.findElement(LOG_WORKOUT_BUTTON).click();
+        log.info("Open Log Workout Page by " + LOG_WORKOUT_BUTTON);
+        return this;
+    }
+
+    @Step("Get text by faker")
+    public String returnNoteFakerText() {
+        log.info("Return text : " + text);
+        return text;
+    }
+
+    @Step("Get Note value")
+    public String getNoteText() throws InterruptedException {
+        String[] value = driver.findElement(NOTE_TEXT).getText().split(":");
+        log.info("Return " + value[1].trim());
+        return value[1].trim();
+    }
+
+    @Step("Create Note")
+    public LogWorkoutPage createNote() {
+        driver.findElement(NOTE_TEXT_AREA).sendKeys(text);
+        log.info("Write " + text + " into Note Text Area by " + NOTE_TEXT_AREA);
+        driver.findElement(CREATE_NOTE_BUTTON).click();
+        log.info("Click Create Note button by " + CREATE_NOTE_BUTTON);
+        return this;
+    }
+
+    @Step("Delete body stats")
+    public void deleteBodyStats() {
+        new IdButton(driver, "delete-body-stats").click();
+    }
+
+    @Step("Click Add Body Stats Button")
+    public LogWorkoutPage clickAddBodyStatsButton() {
+        new IdButton(driver, "edit-body-stats").click();
+        return this;
+    }
+
+    @Step("Click Add Session Summary button")
+    public LogWorkoutPage clickAddSessionSummaryButton() {
+        new IdButton(driver, "add-session").click();
+        return this;
+    }
+
+    @Step("Click Notes Add button")
+    public LogWorkoutPage clickNotesAddButton() {
+        new IdButton(driver, "add-note").click();
+        return this;
+    }
+
+    @Step("Make sure the Summary is created")
+    public boolean summaryIsCreated() {
+        log.info("Find element --> " + SUMMARY_IS_CREATED);
+        return driver.findElement(SUMMARY_IS_CREATED).isDisplayed();
+    }
+
+    @Step("Create {bodyStats}")
+    public LogWorkoutPage createBodyStats(BodyStats bodyStats) {
+        new Input(driver, "weight").write(bodyStats.getWeight());
+        new Input(driver, "fatpercent").write(bodyStats.getBodyFat());
+        new Input(driver, "height").write(bodyStats.getHeight());
+        new Input(driver, "chest").write(bodyStats.getChest());
+        new Input(driver, "waist").write(bodyStats.getWaist());
+        new Input(driver, "arms").write(bodyStats.getArms());
+        new Input(driver, "shoulders").write(bodyStats.getShoulders());
+        new Input(driver, "forearms").write(bodyStats.getForeams());
+        new Input(driver, "neck").write(bodyStats.getNeck());
+        new Input(driver, "hips").write(bodyStats.getHips());
+        new Input(driver, "thighs").write(bodyStats.getThighs());
+        new Input(driver, "calves").write(bodyStats.getCalves());
+        log.info("Create " + bodyStats + "\n and click Done button  ");
+        return clickDoneBodyStats();
+    }
+
+    @Step("Create Session Summary")
+    public LogWorkoutPage createSessionSummary() {
+        int randomHourStart = (int) (1 + Math.random()*24);
+        int randomHourEnd = (int) (1 + Math.random()*24);
+        int randomMinStart = (int) (1 + Math.random()*60);
+        int randomMinEnd = (int) (1 + Math.random()*60);
+        String hourStart = Integer.toString(randomHourStart);
+        String hourEnd = Integer.toString(randomHourEnd);
+        String minStart = Integer.toString(randomMinStart);
+        String minEnd = Integer.toString(randomMinEnd);
+        new Input(driver, "starthour").clear();
+        new Input(driver, "starthour").write(hourStart);
+        new Input(driver, "startmin").clear();
+        new Input(driver, "startmin").write(minStart);
+        new Input(driver, "endhour").clear();
+        new Input(driver, "endhour").write(hourEnd);
+        new Input(driver, "endmin").clear();
+        new Input(driver, "endmin").write(minEnd);
+        log.info("Create Session Summary with random data and click Create Summary button");
+        return clickCreateSummaryButton();
+    }
+
+    @Step("Open Weight page")
+    public LogWorkoutPage moveIntoAWeightBodyPart() {
+        driver.findElement(WEIGHT_BODY_STATS_BUTTON).click();
+        log.info("Click on WEIGHT element by " + WEIGHT_BODY_STATS_BUTTON);
+        return this;
+    }
+
+    @Step("Make sure the weight is opened and back to the Log Workout Page")
+    public boolean weightIsOpenedAndBackToTheLogWorkoutPage() {
+        boolean weightIsOpened = driver.findElement(WEIGHT_IS_OPENED).isDisplayed();
+        log.info("Text \"Body stat progress: weight( lbs):\" found on page");
+        driver.navigate().back();
+        log.info("Go back to the previous page");
+        return weightIsOpened;
+    }
+
+    @Step("Get text at field {fieldName} ")
+    public String getBodyStatsValue(String fieldName) {
+        String bodyStatusValue = driver.findElement(By.xpath(String.format(bodyStatsFieldValue, fieldName))).getText().replaceAll("[^0-9]", "");
+        log.info("Return the text from the field - " + fieldName);
+        return bodyStatusValue;
+    }
+
+    @Step("Click Create Summary button ")
+    public LogWorkoutPage clickCreateSummaryButton() {
+        driver.findElement(CREATE_SUMMARY_BUTTON).click();
+        log.info("Click Create Summary button by " + CREATE_SUMMARY_BUTTON);
+        return this;
+    }
+
+    @Step("Click Done Body Status Button")
+    public LogWorkoutPage clickDoneBodyStats() {
+        new IdButton(driver, "submit-body-stats").click();
+        return this;
+    }
+
+    @Step("Move to Training Stats")
+    public LogWorkoutPage moveToTrainingStats() {
+        new ButtonAHref(driver, "Training Stats").click();
+        new ButtonAHref(driver, "Barbell Bench Press").click();
+        return this;
+    }
+
+    @Step("Make sure the Training is opened")
+    public boolean trainingIsOpened() {
+        log.info("Text \"Mark your favorite exercise. (Click again to undo)\" found on Training page");
+        return driver.findElement(TRAINING_IS_OPENED).isDisplayed();
+    }
+
+    @Override
+    @Step("Find element to make sure the page is open")
+    public boolean isPageOpen() {
+        log.info("Find element --> " + SESSION_SUMMARY_TITLE);
+        return isExist(SESSION_SUMMARY_TITLE);
+    }
+
+
+}
---
+++ src/test/java/tests/LogWorkoutTest.java
@@ -0,0 +1,93 @@
+package tests;
+
+import models.BodyStats;
+import models.BodyStatsFactory;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+public class LogWorkoutTest extends BaseTest {
+
+    @Test(description = "Create Body Stats")
+    public void bodyStatsShouldBeCreatedTest() {
+        loginPage.open()
+                .login();
+        BodyStats bodyStats = BodyStatsFactory.get();
+        boolean isBodyStatsOpened = logWorkoutPage.open()
+                .clickAddBodyStatsButton()
+                .createBodyStats(bodyStats)
+                .isPageOpen();
+
+        assertTrue(isBodyStatsOpened, "Body Stats form hasn't opened");
+
+        assertEquals(logWorkoutPage.getBodyStatsValue("Weight"), bodyStats.getWeight(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Body Fat"), bodyStats.getBodyFat(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Height"), bodyStats.getHeight(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Chest"), bodyStats.getChest(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Waist"), bodyStats.getWaist(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Arms"), bodyStats.getArms(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Shoulders"), bodyStats.getShoulders(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Foreams"), bodyStats.getForeams(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Neck"), bodyStats.getNeck(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Hips"), bodyStats.getHips(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Thighs"), bodyStats.getThighs(), "The text doesn't match");
+        assertEquals(logWorkoutPage.getBodyStatsValue("Calves"), bodyStats.getCalves(), "The text doesn't match");
+
+        logWorkoutPage.deleteBodyStats();
+
+    }
+
+    @Test(description = "Create Session Summary")
+    public void sessionSummaryShouldBeCreatedTest() {
+        loginPage.open()
+                .login();
+
+        logWorkoutPage.open()
+                .clickAddSessionSummaryButton()
+                .createSessionSummary();
+
+        assertTrue(logWorkoutPage.summaryIsCreated(), "Summary is not created");
+    }
+
+    @Test(description = "Create Note")
+    public void noteShouldBeCreatedTest() throws InterruptedException {
+        loginPage.open()
+                .login();
+        logWorkoutPage.open()
+                .clickNotesAddButton()
+                .createNote();
+
+        assertEquals(logWorkoutPage.returnNoteFakerText(), logWorkoutPage.getNoteText(), "The text doesn't match");
+    }
+
+    @Test(description = "Open Weight in Body Part")
+    public void weightBodyPartShouldBeOpenedTest() {
+        loginPage.open()
+                .login();
+        BodyStats bodyStats = BodyStatsFactory.get();
+        boolean weightIsOpened = logWorkoutPage.open()
+                .clickAddBodyStatsButton()
+                .createBodyStats(bodyStats)
+                .moveIntoAWeightBodyPart()
+                .weightIsOpenedAndBackToTheLogWorkoutPage();
+
+        assertTrue(weightIsOpened, "Page is not opened");
+
+        logWorkoutPage.deleteBodyStats();
+
+    }
+
+    @Test(description = "Open training")
+    public void trainingShouldBeOpenedTest() {
+        loginPage.open()
+                .login();
+        boolean trainingIsOpened = logWorkoutPage.moveToTrainingStats()
+                .trainingIsOpened();
+
+        assertTrue(trainingIsOpened, "Page is not opened");
+
+    }
+}
+
+
