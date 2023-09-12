955cc1ee6c61f7036203613137d913d5516936e2
Ivan <ishkredov2@gmail.com>
2023-09-12 16:28:12 +0300

    added FriendsPage and tests

---
+++ src/test/java/pages/FriendsPage.java
@@ -0,0 +1,99 @@
+package pages;
+
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+@Log4j2
+public class FriendsPage extends BasePage {
+    public FriendsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public static final By FRIENDS_SPAN = By.xpath("//li[contains(text(),'Friend')]");
+    public static final By MY_FRIENDS_TITLE = By.xpath("//div[@id='friendsListTab']");
+    public static final By CONTENT_BOX_TEXT = By.xpath("//div[@class='contentBox']/div[contains(@style, 'text')]");
+
+    public static final By BLOCKED_LIST_LINK = By.xpath("//a[@onclick]");
+    public static final By BLOCKED_LIST_TITLE = By.xpath("//div[contains(text(), 'block')]");
+
+    public static final By INCOMING_REQUESTS_TAB = By.xpath("//div[contains(@onclick, 'incomingRequest')]");
+    public static final By INCOMING_REQUESTS_CONTENT = By.xpath("//div[@class='contentBox']/div[contains(text(), 'request')]");
+
+    public static final By SEARCH_FRIENDS_TAB = By.xpath("//div[contains(@onclick, 'searchFriends')]");
+    public static final By SEARCH_FRIENDS_CONTENT = By.xpath("//div[@class='contentBox']//div[normalize-space()='Search and add your friends!']");
+    public static final By SEARCH_FRIENDS_INPUT = By.xpath("//input[@id='suggestionsinput']");
+    public static final By SEARCH_FRIENDS_BUTTON = By.xpath("//input[@id='searchFriend']");
+    public static final By ADD_FRIEND_BUTTON = By.xpath("//a[normalize-space()='+ Add']");
+    public static final By FRIEND_REQUEST_SENT_ACTION = By.xpath("//a[@class='unclickable']");
+
+
+    @Step("Open My Friends")
+    public FriendsPage openMyFriends() {
+        driver.get(BASE_URL + "my-jefit/friends.php");
+        return this;
+    }
+
+    @Step("Open Blocked List")
+    public void openBlockedList() {
+        driver.findElement(BLOCKED_LIST_LINK).click();
+        log.info("Click on blocked list link by " + BLOCKED_LIST_LINK);
+        WebElement blockListIsEmpty = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BLOCKED_LIST_TITLE));
+        log.debug("Explicit Wait element " + BLOCKED_LIST_TITLE);
+    }
+
+    @Step("Open incoming request")
+    public void openIncomingRequests() {
+        driver.findElement(INCOMING_REQUESTS_TAB).click();
+        log.info("Click on element--> " + INCOMING_REQUESTS_TAB);
+        WebElement incomingRequestText = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(INCOMING_REQUESTS_CONTENT));
+        log.debug("Explicit Wait element " + INCOMING_REQUESTS_CONTENT);
+    }
+
+    @Step("Open Search Friends")
+    public FriendsPage openSearchFriends() {
+        driver.findElement(SEARCH_FRIENDS_TAB).click();
+        log.info("Click on element--> " + SEARCH_FRIENDS_TAB);
+        WebElement searchAndAddText = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(SEARCH_FRIENDS_CONTENT));
+        log.debug("Explicit Wait element " + SEARCH_FRIENDS_CONTENT);
+        return this;
+    }
+
+    @Step("Get text from an element content box")
+    public String getContentBoxText() {
+        log.info("Get text from an element --> " + CONTENT_BOX_TEXT);
+        return driver.findElement(CONTENT_BOX_TEXT).getText();
+    }
+
+    @Step("Add new friend")
+    public void addNewFriend() {
+        driver.findElement(SEARCH_FRIENDS_INPUT).sendKeys("mazzuste");
+        log.info("Input 'rivegd' in element --> " + SEARCH_FRIENDS_INPUT);
+        driver.findElement(SEARCH_FRIENDS_BUTTON).click();
+        log.info("Click button search friends by " + SEARCH_FRIENDS_BUTTON);
+        driver.findElement(ADD_FRIEND_BUTTON).click();
+        log.info("Click button add friend by " + ADD_FRIEND_BUTTON);
+
+    }
+
+    @Step("Request sent invitation is displayed")
+    public boolean requestSentInvitationIsDisplayed() {
+        log.info("Find element --> " + FRIEND_REQUEST_SENT_ACTION);
+        return driver.findElement(FRIEND_REQUEST_SENT_ACTION).isDisplayed();
+    }
+
+    @Override
+    @Step("Find element to make sure the page is open")
+    public boolean isPageOpen() {
+        log.info("Find element --> " + MY_FRIENDS_TITLE);
+        return isExist(MY_FRIENDS_TITLE);
+    }
+
+
+}
---
+++ src/test/java/tests/FriendsTest.java
@@ -0,0 +1,71 @@
+
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+public class FriendsTest extends BaseTest {
+
+    @Test(description = "Open friends")
+    public void friendsShouldBeOpenedTest() {
+        loginPage
+                .open()
+                .login();
+        boolean isFriendsPageOpened = friendsPage
+                .openMyFriends()
+                .isPageOpen();
+
+        assertTrue(isFriendsPageOpened, "Friends page hasn't opened");
+    }
+
+    @Test(description = "Open Blocked List")
+    public void blockedListShouldBeOpenedTest() {
+        loginPage
+                .open()
+                .login();
+        friendsPage
+                .openMyFriends()
+                .openBlockedList();
+
+        assertEquals(friendsPage.getContentBoxText(), "The block list is empty.", "Blocked list hasn't opened");
+    }
+
+    @Test(description = "Open Incoming Request")
+    public void incomingRequestShouldBeOpenedTest() {
+        loginPage
+                .open()
+                .login();
+        friendsPage
+                .openMyFriends()
+                .openIncomingRequests();
+
+        assertEquals(friendsPage.getContentBoxText(), "No incoming friend request.", "Incoming Requests tab hasn't opened");
+    }
+
+    @Test(description = "Open Search Friends")
+    public void searchFriendsShouldBeOpenedTest() {
+        loginPage
+                .open()
+                .login();
+        friendsPage
+                .openMyFriends()
+                .openSearchFriends();
+
+        assertEquals(friendsPage.getContentBoxText(), "Search and add your friends!", "Search Friends tab hasn't opened");
+    }
+
+    @Test(description = "Add new friend")
+    public void newFriendShouldBeAdded() {
+        loginPage
+                .open()
+                .login();
+        friendsPage
+                .openMyFriends()
+                .openSearchFriends()
+                .addNewFriend();
+        assertTrue(friendsPage.requestSentInvitationIsDisplayed(), "Invitation to friend hasn't sent");
+    }
+
+}
